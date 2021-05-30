af61da8f245f2ac8bdda1a8be7caccc334393a9a
tushar_android <tushar.pandey@bold.com>
2021-05-30 14:48:47 +0530

    percentage layout added

---
+++ IamRich/features/mvc/QuizViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuizViewController.swift
+//  IamRich
+//
+//  Created by itsupport on 30/05/21.
+//  Copyright © 2021 itsupport. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "QUIZ"
+    }
+}
